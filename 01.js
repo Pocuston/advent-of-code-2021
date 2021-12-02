e78e4f57f2b2d2365aa26c729fe4423e5230a078
Jakub Šlambora <jakub.slambora@unicorn.com>
2021-12-02 23:14:18 +0100

    Day 1 - part 2

--- 01.js
+++ 01.js
@@ -6,12 +6,42 @@ await inputFileHandle.close();
 
 const measurements = inputDataString.split("\r\n").map((str) => parseInt(str));
 
-const totalNumberOfIncreases = measurements.reduce((numberOfIncreases, currentMeasurement, i) => {
-  const previousMeasurement = measurements[i-1];
-  return previousMeasurement && previousMeasurement < currentMeasurement ? numberOfIncreases + 1 : numberOfIncreases;
-}, 0);
+const initialAccumulator = {
+  numberOfIncreases: 0,
+  currentWindow: []
+};
+
+const windowLength = 3;
+
+const { numberOfIncreases: totalNumberOfIncreases } = measurements.reduce(({ currentWindow, numberOfIncreases }, currentMeasurement) => {
+  if (currentWindow.length < windowLength) {
+    return {
+      currentWindow: [...currentWindow, currentMeasurement],
+      numberOfIncreases
+    }
+  }
+  else {
+    const previousWindow = [...currentWindow];
+    const previousWindowSum = sumArrayValues(previousWindow);
+    currentWindow = [...currentWindow.slice(1, windowLength), currentMeasurement];
+    const currentWindowSum = sumArrayValues(currentWindow);
+
+    if (currentWindowSum > previousWindowSum) {
+      numberOfIncreases++;
+    }
+
+    return {
+      currentWindow,
+      numberOfIncreases
+    }
+  }
+}, initialAccumulator);
 
 console.log("Result: ", totalNumberOfIncreases);
+
+function sumArrayValues(array) {
+  return array.reduce((sum, currentValue) => sum + currentValue, 0);
+}
 
 
 
